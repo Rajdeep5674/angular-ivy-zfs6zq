ad2f169fdec546d578dc37d1cc27e01bfaa57a09
Rajdeep5674 <raj.bhadra94@gmail.com>
2020-05-15 15:58:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-zfs6zq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zfs6zq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+  name = 'Angular ' + VERSION.major;
+}
