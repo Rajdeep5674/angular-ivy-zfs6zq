8dbe342b2fdc96ad1012e4929aef244cf376fe02
Rajdeep5674 <raj.bhadra94@gmail.com>
2020-05-26 11:13:45 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -271,4 +271,15 @@ console.log(this.CommentModel);
   this.login_boolean=false;
   }
 
+//functions for collapsed s
+openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
 }
